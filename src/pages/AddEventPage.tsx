518fe31bb99f187183b878d61bf2f3006d7ebec1
Ben Chi <benthechi@gmail.com>
2024-11-22 16:32:23 -0800

    Add Event Page and forms

--- src/pages/AddEventPage.tsx
+++ src/pages/AddEventPage.tsx
@@ -1,12 +1,173 @@
-import { Text } from '@mantine/core';
+import { useState } from 'react';
+import { Button, Card, Center, Code, FileInput, Stack, Text, TextInput } from '@mantine/core';
+import { DateTimePicker } from '@mantine/dates';
+import { hasLength, isEmail, useForm } from '@mantine/form';
+import { MultiSelectCreatable } from '@/components/Inputs/MultiSelectCreatable';
 import { BasicAppShell } from '../components/AppShell/BasicAppShell';
 import { DarkModeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 
+const notExists = [
+  'John',
+  'Steve',
+  'Alice',
+  'Emma',
+  'Ava',
+  'Sophia',
+  'Isabella',
+  'Mia',
+  'Charlotte',
+  'Amelia',
+  'Harper',
+  'Evelyn',
+  'Abigail',
+  'Emily',
+  'Ella',
+  'Elizabeth',
+  'Camila',
+  'Luna',
+  'Sofia',
+  'Avery',
+];
+
+const stylesList = ['Breaking', 'Popping', 'Locking', 'Hip Hop', 'House', 'Waacking'];
+
 export function AddEventPage() {
+  const form = useForm({
+    mode: 'controlled',
+    initialValues: {
+      title: '',
+      datetime: '',
+      address: '',
+      city: '',
+      cost: '',
+      prizes: '',
+      description: '',
+      styles: [],
+      organizers: [],
+      djs: [],
+      mcs: [],
+      videographers: [],
+      photographers: [],
+      sponsors: [],
+      graphicDesigners: [],
+      recapVideo: '',
+      promoVideo: '',
+      images: [],
+    },
+    // validate: {
+    //   name: hasLength({ min: 3 }, 'Must be at least 3 characters'),
+    //   email: isEmail('Invalid email'),
+    // },
+  });
+
+  const [submittedValues, setSubmittedValues] = useState<typeof form.values | null>(null);
+
+  console.log(submittedValues);
+
   return (
     <BasicAppShell>
       <DarkModeToggle />
-      <Text>ADD EVENT FORM</Text>
+
+      <form onSubmit={form.onSubmit(setSubmittedValues)}>
+        <Card radius="md" m="md" withBorder>
+          <Center>Event Info</Center>
+          <TextInput
+            {...form.getInputProps('title')}
+            label="Title (required)"
+            placeholder="ex. Freestyle Session"
+          />
+          <DateTimePicker
+            {...form.getInputProps('datetime')}
+            label="Date & Time (required)"
+            placeholder="ex. 2023-12-31 18:00"
+          />
+          <TextInput
+            {...form.getInputProps('address')}
+            label="Address"
+            placeholder="ex. 123 Dance St."
+          />
+          <TextInput
+            {...form.getInputProps('city')}
+            label="City (required)"
+            placeholder="ex. Los Angeles"
+          />
+          <TextInput {...form.getInputProps('cost')} label="Cost" placeholder="ex. $20" />
+          <TextInput {...form.getInputProps('prizes')} label="Prizes" placeholder="ex. $1000" />
+          <TextInput
+            {...form.getInputProps('description')}
+            label="Description"
+            placeholder="Freestyle session is a celebration of breaking and Hip Hop culture"
+          />
+          <Text>Styles</Text>
+          <MultiSelectCreatable
+            exists={[]}
+            notExists={stylesList}
+            {...form.getInputProps('styles')}
+          />
+        </Card>
+
+        <Card radius="md" m="md" withBorder>
+          <Center>Media</Center>
+          <TextInput
+            {...form.getInputProps('promoVideo')}
+            label="Promo Video"
+            placeholder="www.youtube.com/watch?v=yyyyy"
+          />
+          <TextInput
+            {...form.getInputProps('recapVideo')}
+            label="Recap Video"
+            placeholder="www.youtube.com/watch?v=xxxxxx"
+          />
+          <FileInput
+            {...form.getInputProps('images')}
+            label="Upload images"
+            placeholder="Upload images"
+            multiple
+          />
+        </Card>
+
+        <Card radius="md" m="md" withBorder>
+          <Center>Team</Center>
+          <Text>Organizers</Text>
+          <MultiSelectCreatable
+            exists={[]}
+            notExists={notExists}
+            {...form.getInputProps('organizers')}
+          />
+          <Text>DJ's</Text>
+          <MultiSelectCreatable exists={[]} notExists={notExists} {...form.getInputProps('djs')} />
+          <Text>MC's</Text>
+          <MultiSelectCreatable exists={[]} notExists={notExists} {...form.getInputProps('mcs')} />
+          <Text>Videographers</Text>
+          <MultiSelectCreatable
+            exists={[]}
+            notExists={notExists}
+            {...form.getInputProps('videographers')}
+          />
+          <Text>Photographers</Text>
+          <MultiSelectCreatable
+            exists={[]}
+            notExists={notExists}
+            {...form.getInputProps('photographers')}
+          />
+          <Text>Sponsors</Text>
+          <MultiSelectCreatable
+            exists={[]}
+            notExists={notExists}
+            {...form.getInputProps('sponsors')}
+          />
+          <Text>Graphic Designers</Text>
+          <MultiSelectCreatable
+            exists={[]}
+            notExists={notExists}
+            {...form.getInputProps('graphicDesigners')}
+          />
+        </Card>
+
+        <Button type="submit" mt="md">
+          Submit
+        </Button>
+      </form>
     </BasicAppShell>
   );
 }
--- src/pages/EventPage.tsx
+++ src/pages/EventPage.tsx
@@ -1,10 +1,7 @@
-import { Link } from 'react-router-dom';
-import { Button, Card, Center, Group, Image, Paper, Pill, Stack, Text, Title } from '@mantine/core';
 import { EventProvider } from '@/components/Providers/EventProvider';
 import { BasicAppShell } from '../components/AppShell/BasicAppShell';
 import { DarkModeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Event } from '../components/Event';
-import { BattlesSection } from '../components/Sections/BattlesSection';
 import eventData from '../single-event-test.json';
 
 const allUsers = ['user1', 'user2', 'user3', 'user4', 'user5'];
