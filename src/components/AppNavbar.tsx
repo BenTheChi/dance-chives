d9c08b3c538646c3b32cb7e3cfc1b24665811684
Edro Gonzales <edrogonzales@hotmail.com>
2025-05-19 22:34:21 -0700

    feat: sign up feature

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -4,8 +4,26 @@ import { SearchIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { usePathname } from "next/navigation";
+import {
+    signInWithGoogle,
+    signOutAccount,
+} from "@/lib/server_actions/auth_actions";
+import Image from "next/image";
+
+const menuItems = [
+    { label: "Events", href: "/events" },
+    { label: "Series", href: "/series" },
+    { label: "People", href: "/people" },
+];
 
 export function AppNavbar() {
+    if (usePathname() === "/signup") {
+        return <></>;
+    }
+
+    const isAuthenticated = false;
+
     return (
         <nav className="border-b bg-sidebar px-4">
             <div className="flex h-14 space-between items-center w-full">
@@ -17,31 +35,42 @@ export function AppNavbar() {
                         className="w-full bg-background pl-8 md:w-[200px] lg:w-[300px]"
                     />
                 </div>
-                <div className="flex items-center space-x-8 text-lg font-medium mr-8">
-                    <Link
-                        href="/events"
-                        className="transition-colors hover:text-foreground/80 text-foreground/60"
-                    >
-                        Events
-                    </Link>
-                    <Link
-                        href="/series"
-                        className="transition-colors hover:text-foreground/80 text-foreground/60"
-                    >
-                        Series
-                    </Link>
-                    <Link
-                        href="/people"
-                        className="transition-colors hover:text-foreground/80 text-foreground/60"
-                    >
-                        People
-                    </Link>
+                <div className="flex items-center space-x-8 text-sm font-medium mr-8">
+                    {menuItems.map((item) => (
+                        <Link
+                            key={item.href}
+                            href={item.href}
+                            className="transition-colors hover:text-foreground/80 text-foreground/60"
+                        >
+                            {item.label}
+                        </Link>
+                    ))}
                 </div>
-                <div className="flex items-center space-x-2 ml-auto">
-                    <Button variant="ghost" size="sm">
-                        Login
-                    </Button>
-                    <Button size="sm">Sign up</Button>
+                <div className="flex items-center gap-x-2 ml-auto whitespace-nowrap">
+                    {isAuthenticated ? (
+                        <Button
+                            size="md"
+                            className="w-full cursor-pointer"
+                            onClick={signOutAccount}
+                        >
+                            Logout
+                        </Button>
+                    ) : (
+                        <Button
+                            size="md"
+                            className="w-full cursor-pointer"
+                            onClick={signInWithGoogle}
+                        >
+                            <Image
+                                src="/GLogo.svg"
+                                alt="Google"
+                                width={20}
+                                height={20}
+                                className="mr-2"
+                            />
+                            Sign In
+                        </Button>
+                    )}
                 </div>
             </div>
         </nav>
