76e6107155304db55ef8e9078cc947de54e0567f
Ben Chi <benthechi@gmail.com>
2025-07-08 14:07:40 -0700

    Make changes to add postgres

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,8 @@ import NextAuth, { type DefaultSession } from "next-auth";
 import { Neo4jAdapter } from "@auth/neo4j-adapter";
 import Google from "next-auth/providers/google";
 import driver from "./db/driver";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./lib/primsa";
 
 declare module "next-auth" {
   interface Session {
@@ -17,9 +19,14 @@ declare module "next-auth" {
   }
 }
 
-const neo4jSession = driver.session();
+// const neo4jSession = driver.session();
+
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   providers: [Google],
+//   adapter: Neo4jAdapter(neo4jSession),
+// });
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
-  adapter: Neo4jAdapter(neo4jSession),
 });
