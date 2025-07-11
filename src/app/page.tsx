571453ed8fa08660e20240e4d6a68db37d817958
Ben Chi <benthechi@gmail.com>
2025-07-11 13:20:43 -0700

    Testing dev branch deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,24 @@ import Link from "next/link";
 import { useAuth } from "@/components/providers/AuthProvider";
 
 export default function Home() {
-    const { session } = useAuth();
+  const { session } = useAuth();
 
-    return (
+  return (
+    <div>
+      <AppNavbar />
+      {session ? (
         <div>
-            <AppNavbar />
-            {session ? (
-                <div>
-                    <header>
-                        <h1>Home</h1>
-                        <Link href="/add-event">
-                            <Button>Add Event</Button>
-                        </Link>
-                    </header>
-                    <section>Content here</section>
-                </div>
-            ) : (
-                <div>Not logged in</div>
-            )}
+          <header>
+            <h1>Home Dev Test</h1>
+            <Link href="/add-event">
+              <Button>Add Event</Button>
+            </Link>
+          </header>
+          <section>Content here</section>
         </div>
-    );
+      ) : (
+        <div>Not logged in</div>
+      )}
+    </div>
+  );
 }
